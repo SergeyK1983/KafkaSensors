e980a3c17ff31909029aadd3d95fd8bfbf8c4bda
SergeyK1983 <ssp-serg@yandex.ru>
2025-04-10 22:39:31 +0300

    feat(s1/sensors) Added serializers and fields

--- clusterkafka/sensors/serializers.py
+++ clusterkafka/sensors/serializers.py
@@ -1,19 +1,35 @@
+from django.core.validators import MinValueValidator
 from rest_framework import serializers
+
+
+class FrequencyConverterSerializer(serializers.Serializer):
+    alarm = serializers.BooleanField(default=False, help_text="Тревога/Авария")
 
 
 class ElectricDriveSerializer(serializers.Serializer):
     """ Состояние электропривода """
 
-    name = serializers.CharField(max_length=50)
-    work = serializers.BooleanField()
-    stop = serializers.BooleanField()
-    alarm = serializers.BooleanField()
+    name = serializers.CharField(max_length=50, help_text="Наименование")
+    work = serializers.BooleanField(help_text="В работе")
+    stop = serializers.BooleanField(help_text="Остановлен")
+    alarm = serializers.BooleanField(default=False, help_text="Тревога/Авария")
+    operating_time = serializers.IntegerField(
+        validators=[MinValueValidator(0)], required=False, allow_null=True, help_text="Время наработки, в часах"
+    )
+    is_frequency_converter = serializers.BooleanField(default=False, help_text="Наличие ЧП")
+    frequency_converter = FrequencyConverterSerializer(required=False, many=False)
+
+    def validate_is_frequency_converter(self, attrs):
+        if attrs["frequency_converter"]:
+            if not attrs["is_frequency_converter"]:
+                raise serializers.ValidationError(f"Поле 'is_frequency_converter' должно быть True")
 
 
 class PumpGroupControlMode(serializers.Serializer):
-    """ Режим работы группы насосов """
+    """ Группа насосов, режим работы """
 
-    is_automatic = serializers.BooleanField()
+    name = serializers.CharField(max_length=50, help_text="Наименование")
+    is_automatic = serializers.BooleanField(help_text="Автоматическое управление")
     electric_drivers = ElectricDriveSerializer(many=True)
 
 
@@ -27,7 +43,7 @@ class TelemetrySerializer(serializers.Serializer):
     pass
 
 
-OBJECT_TELEMETRY = {
+OBJECTS_TELEMETRY = {
     "heat_point": TelemetryHeatPointSerializer,
 }
 
