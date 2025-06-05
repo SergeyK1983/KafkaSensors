2959c55d2bb4933c853ab086bc3db6f85cab887c
SergeyK1983 <ssp-serg@yandex.ru>
2025-06-05 22:13:44 +0300

    ref(s4/kafka) refact delete_topics command

--- clusterkafka/sensors/management/commands/create_topics.py
+++ clusterkafka/sensors/management/commands/create_topics.py
@@ -1,7 +1,7 @@
 import logging
 
 from django.core.management.base import BaseCommand, CommandError
-from confluent_kafka.admin import AdminClient
+from confluent_kafka.admin import AdminClient, TopicMetadata
 from confluent_kafka.cimpl import NewTopic, KafkaError, KafkaException
 
 logger = logging.getLogger(__name__)
@@ -13,19 +13,34 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("-tm", "--topic_name", type=str, help="Наименование топика Кафка")
 
+        parser.add_argument("-p", "--partitions", type=int, default=3, help="Количество партиций в топике")
+
+        parser.add_argument(
+            "-rf", "--replication_factor", type=int, default=3, help="Количество реплик, в диапазоне 1..3 или -1"
+        )
+
     def handle(self, *args, **options):
         self.stdout.write(
-            self.style.SUCCESS("Будут создана тема (топик) для Kafka")
+            self.style.SUCCESS("Создается тема (топик) для Kafka")
         )
         if not options["topic_name"]:
             raise CommandError("Error: Должно быть указано наименование создаваемого топика")
 
         name: str = options["topic_name"]
+        partitions: int = options["partitions"]
+        replication_factor: int = options["replication_factor"]
+
+        if replication_factor not in (-1, 1, 2, 3):
+            raise CommandError("Error: Количество реплик должно быть в диапазоне 1..3 или -1")
+
+        # ValueError: num_partitions out of expected range 1..100000 or -1 for broker default
+        if partitions not in range(1, 51) and partitions != -1:
+            raise CommandError("Error: Количество разделов должно быть в диапазоне 1..50 или -1")
 
         topic = NewTopic(
             topic=name,
-            num_partitions=1,
-            replication_factor=3,
+            num_partitions=partitions,
+            replication_factor=replication_factor,
         )
 
         conf: dict = {
@@ -38,10 +53,7 @@ class Command(BaseCommand):
             for name_topic, future in result.items():
                 future.result()
 
-            self.stdout.write(
-                self.style.NOTICE(f"Топики: {result}")
-            )
-            logger.info(f"Создан топик {name}")
+            created_topic: list[str] = list(result.keys())
         except KafkaException as e:
             error: KafkaError = e.args[0]
             self.stderr.write(
@@ -50,8 +62,13 @@ class Command(BaseCommand):
             logger.error("Ошибка: %s, %s, %s" % (error.str(), error.code(), error.name()))
             # INVALID_REPLICATION_FACTOR
         except CommandError as e:
-            self.stderr.write(self.style.ERROR(f"Команда провалилась: {str(e)}"))
+            self.stderr.write(
+                self.style.ERROR(f"Команда провалилась: {str(e)}")
+            )
             logger.error(f"Команда провалилась: {str(e)}")
         else:
-            self.stdout.write(self.style.SUCCESS("Тема (топик) для Kafka создана"))
+            self.stdout.write(
+                self.style.SUCCESS(f"Создана тема (топик): {created_topic}")
+            )
+            logger.info(f"Создан топик {created_topic}")
 
